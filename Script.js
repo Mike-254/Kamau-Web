55fa36148c7d46753dbb9624407b10bde8b3c745
Mike-254 <167530351+Mike-254@users.noreply.github.com>
2024-05-08 12:39:30 +0300

    uploading
    file uploading

---
+++ Script.js
@@ -0,0 +1,21 @@
+// You can add JavaScript functionality if needed
+// For example, smooth scrolling to section links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Function to toggle responsive navigation menu
+function toggleMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
